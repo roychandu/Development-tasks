ea0c0869e1360a3527fe1b15aa314fbeaa61fab8
roychandu <cr717753@gmail.com>
2024-07-29 11:35:46 +0530

    set border

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -4,6 +4,7 @@ const decrese_imgs = document.querySelectorAll(".decrese_img");
 add_btn.forEach((btn, index) => {
     btn.addEventListener("click", () => {
         btn.style.backgroundColor = "hsl(14, 86%, 42%)";
+        btn.style.border = "hsl(14, 86%, 42%)"
         
         if (decrese_imgs[index]) {
             decrese_imgs[index].src = "./assets/images/icon-decrement-quantity.svg";
