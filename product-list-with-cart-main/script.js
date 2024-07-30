8d7513da11d677961e4b6e5b5c3d1f9343e8c338
roychandu <cr717753@gmail.com>
2024-07-30 07:57:33 +0530

    total item calculation done

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -4,32 +4,54 @@ const increse_imgs = document.querySelectorAll(".increse_img");
 const quantities = document.querySelectorAll(".quantity");
 const img_empty = document.querySelector(".cart_item");
 const cart_p = document.querySelector(".cart_p");
+const total_span = document.querySelector(".total_items");
+
+let total_item = 0;
+const addedItems = new Array(add_btn.length).fill(false);
 
 add_btn.forEach((btn, index) => {
     let quantity = 1;
 
     btn.addEventListener("click", () => {
-        btn.style.backgroundColor = "hsl(14, 86%, 42%)";
-        btn.style.border = "hsl(14, 86%, 42%)";
-        btn.style.gap = "2.5vw";
-        
-        decrese_imgs[index].src = "./assets/images/icon-decrement-quantity.svg";
-        quantities[index].innerHTML = quantity;
-        quantities[index].style.color = "hsl(20, 50%, 98%)";
-        img_empty.style.opacity = "0";
-        cart_p.style.opacity = "0";
-        
+        if (!addedItems[index]) {
+            btn.style.backgroundColor = "hsl(14, 86%, 42%)";
+            btn.style.border = "hsl(14, 86%, 42%)";
+            btn.style.gap = "2.5vw";
+            
+            decrese_imgs[index].src = "./assets/images/icon-decrement-quantity.svg";
+            quantities[index].innerHTML = quantity;
+            quantities[index].style.color = "hsl(20, 50%, 98%)";
+            img_empty.style.opacity = "0";
+            cart_p.style.opacity = "0";
+
+            addedItems[index] = true;
+            updateTotalItems();
+        }
     });
 
     increse_imgs[index].addEventListener("click", () => {
-        quantity += 1;
-        quantities[index].innerHTML = quantity;
+        if (addedItems[index]) {
+            quantity += 1;
+            quantities[index].innerHTML = quantity;
+            updateTotalItems();
+        }
     });
 
     decrese_imgs[index].addEventListener("click", () => {
-        if (quantity > 1) {
+        if (addedItems[index] && quantity > 1) {
             quantity -= 1;
             quantities[index].innerHTML = quantity;
+            updateTotalItems();
         }
     });
 });
+
+function updateTotalItems() {
+    total_item = 0;
+    add_btn.forEach((btn, index) => {
+        if (addedItems[index]) {
+            total_item += parseInt(quantities[index].innerHTML);
+        }
+    });
+    total_span.innerHTML = total_item;
+}
